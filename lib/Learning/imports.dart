e8f066fa432302ea4fea87be3494b26b12fedcdc
Mir Faizan <faizan.nitsxr@gmail.com>
2022-08-14 15:38:00 +0530

    Imports edit

--- lib/Learning/imports.dart
+++ lib/Learning/imports.dart
@@ -4,6 +4,8 @@ import 'package:my_app/Learning/multiply.dart'
     as mycode; // here we are importing a code written by someone else.
 // This is an example of Code Reuse.
 import 'dart:convert'; // here dart: means that this package is built in the dart system
+import 'package:http/http.dart' as http; // for HTTP APIs
+import 'package:path/path.dart' as p; // For path
 
 main() {
   print(mycode.multiply());
@@ -18,4 +20,12 @@ main() {
   print('Decoded Bytes: $dbytes');
   String decoded = utf8.decode(dbytes);
   print('Decoded String: $decoded');
+
+  var url = Uri.https('google.com', '');
+  http.get(url).then((response) {
+    print('Response Status Code: ${response.statusCode}');
+    print(response.body);
+  });
+
+  p.join('directory', 'file.txt');
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,1 @@
-import 'package:my_app/Learning/multiply.dart' as mycode;
-
-main() {
-  print('Hello World');
-  print(mycode.multiply());
-  mycode.sayHello();
-}
+main() {}
